f9c305022fecd81e0fefaa026bcb59bd4c2b961e
Victor Evangelista <elliottsmith_steven@hotmail.com>
2025-11-06 20:30:13 -0500

    Poliza

---
+++ Birlik_API/Controllers/PolizaController.cs
@@ -0,0 +1,32 @@
+﻿using Birlik_Api.Data;
+using Birlik_Api.Models.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Birlik_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PolizaController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+
+        public PolizaController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+
+
+        [HttpGet]
+        public IActionResult GetPolizas()
+        {
+            var polizas = _context.Polizas.ToList();
+            return Ok(polizas);
+        }
+    }
+}
