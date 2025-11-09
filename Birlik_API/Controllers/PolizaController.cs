30f63333b35433094eb99faaf1e868cd09c193b0
Birlik <it@birlik.com.pe>
2025-11-09 17:36:07 -0500

    Obteniendo Polizas Madres

--- Birlik_API/Controllers/PolizaController.cs
+++ Birlik_API/Controllers/PolizaController.cs
@@ -1,7 +1,8 @@
 ﻿using Birlik_Api.Data;
-using Birlik_Api.Models.Services;
-using Microsoft.AspNetCore.Http;
+using Birlik_Api.Models;
+using Birlik_Api.Models.Response;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Birlik_Api.Controllers
 {
@@ -9,24 +10,46 @@ namespace Birlik_Api.Controllers
     [ApiController]
     public class PolizaController : ControllerBase
     {
-
         private readonly AppDbContext _context;
-
 
         public PolizaController(AppDbContext context)
         {
             _context = context;
         }
 
+        [HttpGet("cliente/{idCliente}")]
 
-
-
-
-        [HttpGet]
-        public IActionResult GetPolizas()
+        public async Task<IActionResult> ObtenerPolizasConConstanciasAsync(int idCliente)
         {
-            var polizas = _context.Polizas.ToList();
-            return Ok(polizas);
+            var poliza = await (from p in _context.Polizas
+                                join r in _context.Ramos on p.FkRamo equals r.IdRamo into ramas
+                                from ramo in ramas.DefaultIfEmpty()
+                                join c in _context.CompaniaSeguros on p.FkCompania equals c.IdCompaniaSeguro into cias
+                                from comp in cias.DefaultIfEmpty()
+                                join d in _context.Documentos
+                                    on p.IdPoliza equals d.FkPoliza
+                                where p.FkCliente == idCliente
+                                      && EF.Functions.Like(d.TituloDocumento, "%Constancia%")
+                                orderby d.FechaCreacion descending
+                                select new PolizaResponseDTO
+                                {
+                                    IdPoliza = p.IdPoliza,
+                                    NumeroPoliza = p.NumeroPoliza,
+                                    NombreRamo = ramo.DescripcionRamo,
+                                    NombreCompania = comp.NombreCompaniaSeguro,
+                                    VigenciaInicio = p.VigenciaInicio,
+                                    VigenciaFin = p.VigenciaFin,
+                                    Constancia = new ConstanciaDTO
+                                    {
+                                        TituloDocumento = d.TituloDocumento,
+                                        RutaDocumento = d.RutaDocumento
+                                    }
+                                })
+                           .FirstOrDefaultAsync();
+
+            return Ok(poliza);
+
         }
+
     }
 }
---
+++ Birlik_API/Models/Response/PolizaResponseDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace Birlik_Api.Models.Response
+{
+    public class ConstanciaDTO
+    {
+        public string? TituloDocumento { get; set; }
+        public string? RutaDocumento { get; set; }
+    }
+
+    public class PolizaResponseDTO
+    {
+        public int IdPoliza { get; set; }
+        public string? NumeroPoliza { get; set; }
+        public string? NombreRamo { get; set; }
+        public string? NombreCompania { get; set; }
+        public DateOnly? VigenciaInicio { get; set; }
+        public DateOnly? VigenciaFin { get; set; }
+        public ConstanciaDTO? Constancia { get; set; }
+    }
+
+}
